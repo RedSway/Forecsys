dfb7dd0221f7a07de838ebbe449de0d6c64c701b
RedSway <36565752+RedSway@users.noreply.github.com>
2018-11-27 16:09:32 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -3,98 +3,109 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("src/resources/input.txt")));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("src/files/input.txt")));
         String line;
         Map<String, Long[]> map = new HashMap<>();
-        Map<String, Set<Long>> mapUserId = new HashMap<>();
+        Map<String, Set<Long>> uniqCountryId = new HashMap<>();
         List<Country> listCountry = new ArrayList<>();
+
 
         while ((line = reader.readLine()) != null) {
             String[] mas = line.split(";");
 
-            if (mas.length == 3 && checkLong(mas[0]) && checkLong(mas[1])) {
+            if (mas.length == 3 && checkString(mas[1]) && checkString(mas[0])) {
                 String country = mas[2];
-                long count = Long.parseLong(mas[1]);
-                long userId = Long.parseLong(mas[0]);
+                long count = Long.valueOf(mas[1]);
+                long userId = Long.valueOf(mas[0]);
 
-                listCountry.add(new Country(country, userId, count));
+                listCountry.add(new Country(country, count, userId));
             }
         }
         reader.close();
 
+        /*
+        смотрим весь список стран и добавляем в мап ту страну которой еще в ней нет,
+        так же добавляем отдельно уникальный countyId в соответсвующий мап uniqCountryId
+                иначе
+        получаем из uniqCountryId список уникальных countyId для текущей страны, получаем из мап
+        количество уникальных countyId и смотрим, если в списоке уникальных countyId нет подобных countyId
+        , то добавляем его в этот список и увеличиваем счетчик
+        */
         for (Country country : listCountry) {
-            String countryy = country.getCountry();
-            long count = country.getCount();
-            long userId = country.getUserId();
-
-            if (!map.containsKey(countryy)) {
-                Set<Long> set = new HashSet<>();
-                set.add(userId);
-                map.put(countryy, new Long[]{count, (long) set.size()});
-                mapUserId.put(countryy, set);
+            if (!map.containsKey(country.getCountry())) {
+                uniqCountryId.put(country.getCountry(), new HashSet<>());
+                uniqCountryId.get(country.getCountry()).add(country.getUserId());
+                map.put(country.getCountry(), new Long[]{country.getCount(), 1L});
             } else {
-                Long[] mas = map.get(countryy);
-                mas[0] = mas[0] + count;
-                Set<Long> set = mapUserId.get(countryy);
-                set.add(userId);
-                mas[1] = (long) set.size();
-                map.put(countryy, mas);
+
+                Set<Long> sets = uniqCountryId.get(country.getCountry());
+                Long[] longs = map.get(country.getCountry());
+                long id;
+                if (sets.contains(country.getUserId()))
+                    id = longs[1];
+                else {
+                    id = longs[1] + 1;
+                    uniqCountryId.get(country.getCountry()).add(country.getUserId());
+                }
+
+                map.put(country.getCountry(), new Long[]{country.getCount() + longs[0], id});
             }
         }
 
         StringBuilder stringBuilder = new StringBuilder();
         for (Map.Entry<String, Long[]> pair : map.entrySet()) {
-            stringBuilder.append(pair.getKey()).append(" ").append(pair.getValue()[0]).append(" ").append(pair.getValue()[1]).append("\n");
+            stringBuilder.append(pair.getKey()).append(";").append(pair.getValue()[0]).append(";").append(pair.getValue()[1]).append("\n");
         }
 
-        writeFile(stringBuilder.toString(), false);
+        fileWrite(stringBuilder.toString(), false);
 
-        /////////////////////////////////////////////
+        ////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////
 
-        reader = new BufferedReader(new InputStreamReader(new FileInputStream("src/resources/result.txt")));
-        List<String[]> listResult = new ArrayList<>();
+        List<String[]> sortList = new ArrayList<>();
+        boolean flag = false;
+        reader = new BufferedReader(new InputStreamReader(new FileInputStream("src/files/result.txt")));
+
         while ((line = reader.readLine()) != null) {
-            String[] mas = line.split(" ");
-
-            if (mas.length == 3 && checkLong(mas[1]) && checkLong(mas[2])) {
-                listResult.add(mas);
+            if (!flag)
+                flag = true;
+            else {
+                String[] mas = line.split(";");
+                sortList.add(mas);
             }
         }
         reader.close();
 
-        listResult.sort(new Comparator<String[]>() {
-            @Override
-            public int compare(String[] o1, String[] o2) {
-                if (!o1[1].equals(o2[1]))
-                    return (int) (Long.parseLong(o1[1]) - Long.parseLong(o2[1]));
-                else
-                    return (int) (Long.parseLong(o1[2]) - Long.parseLong(o2[2]));
-            }
+        sortList.sort((o1, o2) -> {
+            if (!o1[1].equals(o2[1]))
+                return Integer.parseInt(o1[1]) - Integer.parseInt(o2[1]);
+            else
+                return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]);
         });
 
         stringBuilder = new StringBuilder();
-        for (String[] strings : listResult) {
-            stringBuilder.append(strings[0]).append(" ").append(strings[1]).append(" ").append(strings[2]).append("\n");
+        for (String[] strings : sortList) {
+            stringBuilder.append(strings[0]).append(";").append(strings[1]).append(";").append(strings[2]).append("\n");
         }
 
-        writeFile(stringBuilder.toString(), true);
+        fileWrite(stringBuilder.toString(), true);
     }
 
-    private static boolean checkLong(String s) {
+    private static boolean checkString(String string) {
         try {
-            Long.parseLong(s);
-        } catch (NumberFormatException e) {
+            Integer.parseInt(string);
+        } catch (Exception e) {
             return false;
         }
         return true;
     }
 
-    private static void writeFile(String s, boolean flag) {
-        try (FileWriter writer = new FileWriter("src/resources/result.txt", flag)){
-            writer.write("country sum(count) count_uniq(user_id)\n");
-            writer.write(s);
-            writer.write("-------------\n");
-            writer.flush();
+    private static void fileWrite(String s, boolean flag) {
+        try (FileWriter fileWriter = new FileWriter("src/files/result.txt", flag)) {
+            fileWriter.write("country;sum(count);count_uniq(user_id)\n");
+            fileWriter.write(s);
+
+            fileWriter.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
